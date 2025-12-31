8f9a78cb822abc8c31c0a1cb20f776b1e3bbfab1
ExpeditiosReadr <owenw3301@gmail.com>
2025-12-30 19:33:41 -0600

    added bootloader

---
+++ bootstrap.lua
@@ -0,0 +1,4 @@
+local version = 0
+local programs = {}
+table.insert(programs, {"Hello World", "https://raw.githubusercontent.com/ExpeditiousReadr/turtles/refs/heads/main/hello.lua"})
+print("initiating bootloader version " .. version .. ": " .. #programs .. " available.")
